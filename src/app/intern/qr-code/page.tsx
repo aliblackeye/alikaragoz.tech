1c1d32a1fd09bcb5f0a5b84a4287dced1ba68a77
aliblackeye <ali.blackeye@hotmail.com>
2023-04-26 23:40:29 +0300

    fix:backtick fix

--- src/app/intern/qr-code/page.tsx
+++ src/app/intern/qr-code/page.tsx
@@ -11,11 +11,11 @@ export default function QrCode() {
       </div>
       <div className="qr-bottom">
         <p className="qr-title">
-          Improve your front-end skills by building projects
+          {`Improve your front-end skills by building projects`}
         </p>
         <p className="qr-desc">
-          Scan the QR code to visit Ali Karagoz's github account's repositories
-          for giving stars.
+          {`Scan the QR code to visit Ali Karagoz's github account's repositories
+          for giving stars.`}
         </p>
       </div>
     </div>
