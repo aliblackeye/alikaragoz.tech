6aec822d8ff48d79addc1497156ea52996894347
aliblackeye <ali.blackeye@hotmail.com>
2023-04-26 15:31:59 +0300

    fix: spacing

--- src/app/newbie/interactive-rating/page.tsx
+++ src/app/newbie/interactive-rating/page.tsx
@@ -18,13 +18,12 @@ export default function InteractiveRating() {
       {rating ? (
         <div className="interactive-rating rated">
           <div className="rating-image">
-          <Image src={img} alt="phone-and-credit-illustrate"  />
+            <Image src={img} alt="phone-and-credit-illustrate" />
           </div>
           <p className="selected-rating">{`You selected ${rating} out of 5`}</p>
           <h1 className="selected-rating-title">Thank you!</h1>
           <p className="rated-description">
-            We appreciate you taking the time to give a rating. if you ever need
-            more support. don't hesitate to get in touch!
+            We appreciate you taking the time to give a rating. if you ever need more support. Don't hesitate to get in touch!
           </p>
         </div>
       ) : (
